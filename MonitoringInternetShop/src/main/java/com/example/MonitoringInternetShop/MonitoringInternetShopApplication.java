4d35dc8001eee4242ee4dc2d1b59660cb2436d48
Даниил <90754791+Heimdall-Program@users.noreply.github.com>
2023-05-10 22:02:24 +0300

    init

---
+++ MonitoringInternetShop/src/main/java/com/example/MonitoringInternetShop/MonitoringInternetShopApplication.java
@@ -0,0 +1,13 @@
+package com.example.MonitoringInternetShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitoringInternetShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonitoringInternetShopApplication.class, args);
+	}
+
+}
---
+++ MonitoringInternetShop/src/test/java/com/example/MonitoringInternetShop/MonitoringInternetShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MonitoringInternetShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitoringInternetShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
