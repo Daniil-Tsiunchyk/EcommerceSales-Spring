19638b4f766962b67cf495e77a251f887b927997
Даниил <90754791+Heimdall-Program@users.noreply.github.com>
2023-05-23 02:13:30 +0300

    Update dashboard

--- MonitoringInternetShop/src/main/java/com/example/MonitoringInternetShop/Controllers/DashboardController.java
+++ MonitoringInternetShop/src/main/java/com/example/MonitoringInternetShop/Controllers/DashboardController.java
@@ -12,7 +12,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Controller
 public class DashboardController {
@@ -43,10 +46,22 @@ public class DashboardController {
                 ? totalRevenue.divide(BigDecimal.valueOf(latestOrders.size()), 2, RoundingMode.HALF_UP)
                 : BigDecimal.ZERO;
 
+        List<Map<String, Object>> salesData = topProducts.stream()
+                .map(product -> {
+                    Map<String, Object> map = new HashMap<>();
+                    map.put("name", product.getName());
+                    map.put("sales", product.getSales());
+                    map.put("revenue", product.getPrice().multiply(new BigDecimal(product.getSales()))); // new line
+                    return map;
+                })
+                .collect(Collectors.toList());
+
         model.addAttribute("totalSoldProducts", totalSoldProducts);
         model.addAttribute("totalRevenue", totalRevenue);
         model.addAttribute("averageCheck", averageCheck);
+        model.addAttribute("salesData", salesData);
 
         return "dashboard";
     }
+
 }
