d3dd92ed4e4cbd1f12fde37f08e98f271c07dc7b
Даниил <90754791+Heimdall-Program@users.noreply.github.com>
2023-05-22 23:20:10 +0300

    Add security

---
+++ MonitoringInternetShop/src/main/java/com/example/MonitoringInternetShop/Secure/AdminCheckInterceptor.java
@@ -0,0 +1,23 @@
+package com.example.MonitoringInternetShop.Secure;
+
+import com.example.MonitoringInternetShop.Models.User;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+
+public class AdminCheckInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+            throws Exception {
+        HttpSession session = request.getSession();
+        User user = (User) session.getAttribute("user");
+        if (user == null || !user.getRole().equals("ADMIN")) {
+            response.sendRedirect("/login");
+            return false;
+        }
+        return true;
+    }
+}
